421a1486f5df36386ba50fb509200fe29a677873
Pelumi Babalola <Pelumi.Babalola@softwire.com>
2025-07-08 14:20:12 +0100

    added 'fizz' and 'buzz' rules

--- main.go
+++ main.go
@@ -1,10 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func fizzBuzz(i int) string {
+	var output []string
+	if i%3 == 0 {
+		output = append(output, "Fizz")
+	}
+	if i%5 == 0 {
+		output = append(output, "Buzz")
+	}
+
+	if len(output) == 0 {
+		return strconv.Itoa(i)
+	} else {
+		return strings.Join(output, "")
+	}
+}
+
+func getNumber() (int, error) {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print("Enter a maximum number >> ")
+	scanner.Scan()
+	return strconv.Atoi(scanner.Text())
+}
 
 // This is our main function, this executes by default when we run the main package.
 func main() {
-	fmt.Println("Hello, World!")
+	num, err := getNumber()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	for i := 0; i <= num; i++ {
+		fmt.Println(fizzBuzz(i))
+	}
 
-	// Put your code here...
 }
---
+++ main_test.go
@@ -0,0 +1,25 @@
+package main
+
+import "testing"
+
+type fizzBuzzTest struct {
+	input          int
+	expectedOutput string
+}
+
+var fizzBuzzTests = []fizzBuzzTest{
+	{1, "1"},
+	{3, "Fizz"},
+	{5, "Buzz"},
+	{15, "FizzBuzz"},
+}
+
+func TestFizzBuzz(t *testing.T) {
+	for _, test := range fizzBuzzTests {
+		result := fizzBuzz(test.input)
+		if result != test.expectedOutput {
+			t.Errorf("FizzBuzz(%d): Expected '%s', got '%s'", test.input, test.expectedOutput, result)
+
+		}
+	}
+}
